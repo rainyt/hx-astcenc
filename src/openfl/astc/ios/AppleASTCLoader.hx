f8385286d47b677eb947d8ca819a9b9a9212e0f8
Rainy <939029835@qq.com>
2023-12-28 06:45:36 +0800

    更新翻转问题

--- src/openfl/astc/ios/AppleASTCLoader.hx
+++ src/openfl/astc/ios/AppleASTCLoader.hx
@@ -31,9 +31,13 @@ class AppleASTCLoader {
 		untyped __cpp__('CGImageRef source = {0}', image.CGImage());
 		var data:NSMutableData = NSMutableData.data();
 		untyped __cpp__('CGImageDestinationRef destination = {0}',
-			untyped __cpp__('CGImageDestinationCreateWithData((CFMutableDataRef){0}, (CFStringRef)@"org.khronos.ktx", 1, nil)', data));
+			untyped __cpp__('CGImageDestinationCreateWithData((CFMutableDataRef){0}, (CFStringRef)@"org.khronos.astc", 1, nil)', data));
 		var properties:NSDictionary = NSDictionary.fromDynamic({
-			"kCGImagePropertyASTCBlockSize": 0x88
+			"kCGImagePropertyASTCFlipVertically": false,
+			"kCGImagePropertyASTCBlockSize": 0x88,
+			"kCGImageDestinationLossyCompressionQuality": 0
+			// "kCGImagePropertyHasAlpha": true,
+			// "kCGImageAlphaInfo": "at_alpha_premultiplied"
 		});
 		untyped __cpp__('
         CGImageDestinationAddImage({0}, {1}, (CFDictionaryRef){2});
@@ -41,85 +45,7 @@ class AppleASTCLoader {
 		var astcNSData:NSData = untyped data;
 		var astcBytes = astcNSData.toBytes();
 		trace("astcBytes.length", astcBytes.length);
-		var headerBytes = Bytes.alloc(16);
-		// 魔数
-		headerBytes.set(0, 0x13);
-		headerBytes.set(1, 0xAB);
-		headerBytes.set(2, 0xA1);
-		headerBytes.set(3, 0x5C);
-		// Block
-		headerBytes.set(4, 8);
-		headerBytes.set(5, 8);
-		//
-		var textureWidth = Math.floor(image.size.width);
-		var textureHeight = Math.floor(image.size.height);
-		trace("textureSize", textureWidth, textureHeight);
-		headerBytes.set(6, textureWidth & 0xFF);
-		headerBytes.set(7, (textureWidth >> 8) & 0xFF);
-		headerBytes.set(8, (textureWidth >> 16) & 0xFF);
-		headerBytes.set(9, textureHeight & 0xFF);
-		headerBytes.set(10, (textureHeight >> 8) & 0xFF);
-		headerBytes.set(11, (textureHeight >> 16) & 0xFF);
-		headerBytes.set(12, 1);
-		headerBytes.set(13, 0);
-		headerBytes.set(14, 0);
-		headerBytes.set(15, 0);
-		var allBytes:BytesBuffer = new BytesBuffer();
-		allBytes.addBytes(headerBytes, 0, headerBytes.length);
-		allBytes.addBytes(astcBytes, 0, astcBytes.length);
-		trace("allBytes", allBytes.length);
-		return allBytes.getBytes();
+		return astcBytes;
 	}
-
-	/**
-	 * 将纹理转换成ASTC纹理
-	 * @param texture 
-	 */
-	// public static function convertTextureToASTC(texture:Dynamic):Void {
-	// 	untyped __cpp__('
-	// 	// 创建一个MTLTextureDescriptor对象，用来描述astc压缩纹理的属性
-	// 	MTLTextureDescriptor *textureDescriptor = [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatASTC_8x8_sRGB width:texture.width height:texture.height mipmapped:NO];
-	// 	// 创建一个MTLTexture对象，用来存储astc压缩纹理的数据
-	// 	id<MTLTexture> compressedTexture = [self.device newTextureWithDescriptor:textureDescriptor];
-	// 	// 创建一个MTLBlitCommandEncoder对象，用来执行纹理的复制和转换操作
-	// 	id<MTLCommandBuffer> commandBuffer = [self.commandQueue commandBuffer];
-	// 	id<MTLBlitCommandEncoder> blitCommandEncoder = [commandBuffer blitCommandEncoder];
-	// 	// 将原始纹理的数据复制并转换到astc压缩纹理中
-	// 	[blitCommandEncoder copyFromTexture:texture sourceSlice:0 sourceLevel:0 sourceOrigin:MTLOriginMake(0, 0, 0) sourceSize:MTLSizeMake(texture.width, texture.height, 1) toTexture:compressedTexture destinationSlice:0 destinationLevel:0 destinationOrigin:MTLOriginMake(0, 0, 0)];
-	// 	// 结束纹理的复制和转换操作
-	// 	[blitCommandEncoder endEncoding];
-	// 	// 提交命令缓冲区并等待完成
-	// 	[commandBuffer commit];
-	// 	[commandBuffer waitUntilCompleted];
-	// 	// 创建一个NSData对象，用来存储astc压缩纹理的数据
-	// 	NSUInteger dataSize = compressedTexture.width * compressedTexture.height;
-	// 	NSMutableData *data = [NSMutableData dataWithLength:dataSize];
-	// 	// 将astc压缩纹理的数据复制到NSData对象中
-	// 	[compressedTexture getBytes:data.mutableBytes bytesPerRow:compressedTexture.width fromRegion:MTLRegionMake2D(0, 0, compressedTexture.width, compressedTexture.height) mipmapLevel:0];
-	// 	// 创建一个NSData对象，用来存储astc文件的头部信息
-	// 	NSMutableData *header = [NSMutableData dataWithLength:16];
-	// 	uint8_t *headerBytes = header.mutableBytes;
-	// 	// 设置astc文件的魔数
-	// 	headerBytes[0] = 0x13;
-	// 	headerBytes[1] = 0xAB;
-	// 	headerBytes[2] = 0xA1;
-	// 	headerBytes[3] = 0x5C;
-	// 	// 设置astc文件的块大小
-	// 	headerBytes[4] = 8;
-	// 	headerBytes[5] = 8;
-	// 	// 设置astc文件的纹理维度
-	// 	headerBytes[6] = compressedTexture.width & 0xFF;
-	// 	headerBytes[7] = (compressedTexture.width >> 8) & 0xFF;
-	// 	headerBytes[8] = (compressedTexture.width >> 16) & 0xFF;
-	// 	headerBytes[9] = compressedTexture.height & 0xFF;
-	// 	headerBytes[10] = (compressedTexture.height >> 8) & 0xFF;
-	// 	headerBytes[11] = (compressedTexture.height >> 16) & 0xFF;
-	// 	headerBytes[12] = 1;
-	// 	headerBytes[13] = 0;
-	// 	headerBytes[14] = 0;
-	// 	headerBytes[15] = 0;
-	// 	// 将astc文件的头部信息和数据拼接起来
-	// 	[header appendData:data]');
-	// }
 }
 #end
