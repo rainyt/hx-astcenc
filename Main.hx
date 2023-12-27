be7b52e007a460515f3725b2a6e274faf4cd5c8e
Rainy <939029835@qq.com>
2023-12-27 10:12:27 +0800

    新增异常处理

---
+++ Main.hx
@@ -0,0 +1,5 @@
+class Main {
+	static function main() {
+		Sys.exit(Sys.command("./bin/cpp/Tools", Sys.args()));
+	}
+}
--- encode_src/Main.hx
+++
@@ -1,5 +0,0 @@
-class Main {
-	static function main() {
-		Sys.command("./bin/cpp/Tools", Sys.args());
-	}
-}
